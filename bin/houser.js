9c6fd389f1bf43b5151304c24e395de572398df2
Houser <houserqu@qq.com>
2017-09-06 16:32:17 +0800

    创建基本命令

--- bin/houser.js
+++ bin/houser.js
@@ -1,4 +1,11 @@
-var args =process.argv.slice(2);
-var what = args.join(' ');
+var program = require('commander');
+var createreact  = require('../lib/react/index');
 
-console.log(what);
+program
+  .version('0.0.1')
+  .option('-react, --react [new]', 'new of customers in CSV file')
+  .option('-vue, --vue [new]', 'new of customers in CSV file')
+  .parse(process.argv)
+
+if(program.react) createreact();
+if(program.vue) console.log('vue');
